feff3c94f002d0b852e4c0697faa9e6515a62231
Sai <ammu.416@gmail.com>
2020-02-02 17:10:43 -0600

    completed setting up the many-to-many

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,13 +1,12 @@
 package org.launchcode.codingevents.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Event extends AbstractEntity {
@@ -31,6 +30,9 @@ public class Event extends AbstractEntity {
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name,String location,int noOfPeople ,EventCategory eventCategory) {
         this.name = name;
@@ -82,6 +84,14 @@ public class Event extends AbstractEntity {
         this.eventCategory = eventCategory;
     }
 
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTags(Tag tag){
+        this.tags.add(tag);
+
+    }
 
     @Override
     public String toString() {

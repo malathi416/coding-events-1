a00ea2345562ed54e1a6c9d2d320aa83f045960f
Sai <ammu.416@gmail.com>
2020-02-02 14:50:52 -0600

    completed setting up the relationship one-to-one

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,10 @@
 package org.launchcode.codingevents.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Email;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -14,12 +16,10 @@ public class Event extends AbstractEntity {
     @Size(min = 3,max = 50,message = "Name must be between 3 and 50 characters")
     private String name;
 
-    @Size(max = 500 ,message = "Description is too long")
-    private String description;
-
-    @NotBlank(message="Email is required")
-    @Email(message = "Invalid Email.Try again")
-    private String contactEmail;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
 
     @NotBlank(message = "Location is required")
     @Size(max = 50)
@@ -32,11 +32,9 @@ public class Event extends AbstractEntity {
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event(String name,String description,String location,String contactEmail,int noOfPeople ,EventCategory eventCategory) {
+    public Event(String name,String location,int noOfPeople ,EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
         this.location = location;
-        this.contactEmail = contactEmail;
         this.noOfPeople = noOfPeople;
         this.eventCategory = eventCategory;
     }
@@ -51,20 +49,13 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
+
+    public EventDetails getEventDetails() {
+        return eventDetails;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
     }
 
     public String getLocation() {
@@ -90,6 +81,7 @@ public class Event extends AbstractEntity {
     public void setEventCategory(EventCategory eventCategory) {
         this.eventCategory = eventCategory;
     }
+
 
     @Override
     public String toString() {
